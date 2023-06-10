a268c31399c0bb4f835b0c7e7e77bd00f9fce834
Daniel Ferraz <mathioli@yahoo.com>
2023-06-10 11:38:31 -0400

    test commit

--- testmap/ContentView.swift
+++ testmap/ContentView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 import MapKit
-
+//test
 struct ContentView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.3352, longitude: -122.0096), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     
